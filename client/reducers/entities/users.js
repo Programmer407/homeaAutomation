0e8b5b7f8a10f64088795ac7bff7c008ff555b67
Muhammad Kasim <muhammad.kasim23@gmail.com>
2017-07-31 19:57:14 +0500

    Made minor bug fixes

--- client/reducers/entities/users.js
+++ client/reducers/entities/users.js
@@ -7,17 +7,20 @@ export default function users(state = {}, action) {
     return {...state, isLoading: true}
   }
   case ActionTypes.USER_LOGIN_SUCCESS: {
-		const {payload} = action;
-		const {user} = payload;
-		if (!payload) {
+		if (!action.payload) {
       throw new Error(`Can't execute ${ ActionTypes.USER_LOGIN_SUCCESS }. {payload} isn't available in action`)
-    }
-
-		return {...state, user, isLoading: false}
-    /* const { payload: { user } } = action
-    return mergeNewEntities(state, [user], ENTITY_STATUS_DATA_AVAILABLE) */
+		}
+		
+     const { payload: { user } } = action
+    return mergeNewEntities(state, [user], ENTITY_STATUS_DATA_AVAILABLE) 
   }
   case ActionTypes.USER_LOGIN_FAILURE: {
+    return {...state, isLoading: false}
+  }
+  case ActionTypes.USER_LOGOUT_SUCCESS: {
+    return {...state, isLoading: false}
+  }
+  case ActionTypes.USER_LOGOUT_FAILURE: {
     return {...state, isLoading: false}
   }
   /*case ActionTypes.USER_REGISTER_SUCCESS: {
