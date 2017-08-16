7cb9737e3dc7b924f61355d723f25cffaf0bbfad
Majid Hussain <Majid.hussain@emumba.com>
2017-08-16 23:01:52 +0500

    Stagging Environment
    Signed-off-by: Majid Hussain <Majid.hussain@emumba.com>

--- config/app.production.config.js
+++ config/app.production.config.js
@@ -17,8 +17,8 @@ export default {
         host: '172.16.2.103:4000'
     },
     app: {
-        host: 'ec2-52-53-128-32.us-west-1.compute.amazonaws.com',
-        // host: 'localhost',
+        // host: 'ec2-52-53-128-32.us-west-1.compute.amazonaws.com',
+        host: 'localhost',
         serveDummyStatusData: true
     }
 }
